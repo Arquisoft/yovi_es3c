34993be61fc5f1be65a648a7189e8100ec2806e0
uo300069 <uo300069@uniovi.es>
2026-03-08 14:00:29 +0100

    Registrado el nuevo algoritmo en la carpeta de bot_server para poder ser utilizado.

--- gamey/src/bot/mod.rs
+++ gamey/src/bot/mod.rs
@@ -6,10 +6,13 @@
 //! - [`YBot`] - A trait that defines the interface for all bots
 //! - [`YBotRegistry`] - A registry for managing multiple bot implementations
 //! - [`RandomBot`] - A simple bot that makes random valid moves
+//! - [`HeuristicBot`] - Bot that makes coherent moves.
 
 pub mod random;
+pub mod heuristic;
 pub mod ybot;
 pub mod ybot_registry;
 pub use random::*;
+pub use heuristic::*;
 pub use ybot::*;
 pub use ybot_registry::*;
