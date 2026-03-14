22a8390feab3641011b8dabf8d4ab69feb8e4c7f
Tisenka_2003 <uo288816@uniovi.es>
2026-03-14 17:37:03 +0100

    Revision del login

--- webapp/src/pages/Login.tsx
+++ webapp/src/pages/Login.tsx
@@ -74,7 +74,6 @@ const Login: React.FC = () => {
           Iniciar Sesión
         </button>
       </form>
-
       <p>
         ¿No tienes cuenta? <Link to="/register">Regístrate aquí</Link>
       </p>
