fa635cda0707437f4e3fc69572e5db4f946e8266
UO289510 <UO289510@uniovi.es>
2026-03-29 19:38:06 +0200

    Comenzada la implementación de los tests del registro

--- webapp/test/e2e/features/register.feature
+++ webapp/test/e2e/features/register.feature
@@ -5,3 +5,8 @@ Feature: Register
     Given the register page is open
     When I enter "Alice" as the username and submit
     Then I should see a welcome message containing "Hello Alice"
+
+  Scenario: Failed registration, duplicated username
+    Given the register page is open
+    When I enter "pruebina" as a duplicated username and submit
+    Then I should see an error message containing "El nombre de usuario ya está en uso"
--- webapp/test/e2e/steps/register.steps.mjs
+++ webapp/test/e2e/steps/register.steps.mjs
@@ -26,6 +26,18 @@ When('I enter {string} as the username and submit', async function (username) {
   await page.click('.submit-button')
 })
 
+When('I enter {string} as a duplicated username and submit', async function (username) {
+  const page = this.page
+  if(!page) throw new Error('Page not initialized')
+  
+  // Llenamos todos los campos del registro
+  await page.fill('#username', username)
+  await page.fill('#password', 'TestPassword123!')
+  await page.fill('#confirmPassword', 'TestPassword123!')
+
+  await page.click('.submit-button')
+})
+
 Then('I should see a welcome message containing {string}', async function (expected) {
   const page = this.page
   if (!page) throw new Error('Page not initialized')
@@ -43,4 +55,17 @@ Then('I should see a welcome message containing {string}', async function (expec
     content.includes(registeredUsername),
     `Expected page to include registered username "${registeredUsername}"`
   )
+})
+
+Then('I should see an error message containing {string}', async function(message) {
+  const page = this.page
+  if (!page) throw new Error('Page not initialized')
+
+  // Esperar a que aparezca el mensaje de error
+  const content = await page.content()
+  assert.ok(
+    content.includes(message), 
+    `Expected page to include error message: "${message}"`
+  )
+
 })
